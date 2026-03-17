a03592d06e21f35b3b9206ad7675d18e13eabf28
YashasviYadav81 <yashasviyadav609@gmail.com>
2026-03-17 17:45:37 +0530

    Add files via upload

---
+++ c++.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <conio.h>
+using namespace std; // namespace is a conatainer that contains allmost all the fixed names by c++
+int main(){
+    // this is single line comment
+/*this is 
+a multiline comment*/
+int x=3;
+cout<<"Hello";
+cin >>x;// same meaning as scanf in c use cin
+
+cout<<x;
+const double pi=3.14;
+
+}
