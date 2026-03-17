cf3ba5958e463b74ace83305e35f1178c72ca51f
YashasviYadav81 <yashasviyadav609@gmail.com>
2026-03-17 17:45:21 +0530

    Add files via upload

---
+++ c.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h> 
+int main(){
+// this is single line comment
+/*this is 
+a multiline comment*/
+printf("Hello");
+scanf("Enter the number");
+int x=3;
+printf("%d\n", x);
+scanf("%d\n", &x);
+ const double pi=3.14;// you cannot update the value of a constant variable
+}
+
